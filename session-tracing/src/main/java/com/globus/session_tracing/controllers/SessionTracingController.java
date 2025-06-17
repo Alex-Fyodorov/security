dbf72703227cebf06ed13200814de3b99669313f
Alex <rassylka82@inbox.ru>
2025-06-17 14:00:11 +0300

    Написал сваггер

--- session-tracing/src/main/java/com/globus/session_tracing/controllers/SessionTracingController.java
+++ session-tracing/src/main/java/com/globus/session_tracing/controllers/SessionTracingController.java
@@ -3,9 +3,15 @@ package com.globus.session_tracing.controllers;
 import com.globus.session_tracing.converters.SessionConverter;
 import com.globus.session_tracing.dtos.SessionDto;
 import com.globus.session_tracing.entities.Session;
+import com.globus.session_tracing.exceptions.AppError;
 import com.globus.session_tracing.services.SessionTracingService;
 import com.globus.session_tracing.validators.SessionValidator;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +22,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/sessions")
+@Tag(name = "Сессии пользователей", description = "Отслеживание и методы работы с пользовательскими сессиями")
 @RequiredArgsConstructor
 public class SessionTracingController {
     private final SessionTracingService sessionTracingService;
@@ -23,13 +30,20 @@ public class SessionTracingController {
     private final SessionValidator sessionValidator;
 
     @GetMapping
+    @Operation(summary = "Запрос на получение страницы с сессиями",
+            responses = {
+                    @ApiResponse(
+                            description = "Успешный ответ", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = Page.class))
+                    )
+            })
     public Page<SessionDto> findAll(
             @RequestParam(required = false, name = "user_id") @Parameter(
                     description = "Фильтр по id пользователя") Integer userId,
             @RequestParam(required = false, name = "min_login_time") @Parameter(
-                    description = "Фильтр по минимальному времени вхождения в аккаунт") LocalDateTime minLoginTime,
+                    description = "Фильтр по времени входа в аккаунт") LocalDateTime minLoginTime,
             @RequestParam(required = false, name = "method") @Parameter(
-                    description = "Фильтр по методу вхождения в аккаунт") String method,
+                    description = "Фильтр по методу входа в аккаунт") String method,
             @RequestParam(required = false, name = "is_active") @Parameter(
                     description = "Фильтр по активности сессии") Boolean isActive,
             @RequestParam(defaultValue = "1", name = "page") @Parameter(
@@ -41,48 +55,123 @@ public class SessionTracingController {
     }
 
     @GetMapping("/{id}")
-    public SessionDto read(@PathVariable long id) {
+    @Operation(summary = "Запрос на получение сессии по id",
+            responses = {
+                    @ApiResponse(
+                            description = "Успешный ответ", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = SessionDto.class))
+                    ),
+                    @ApiResponse(
+                            description = "Сессия не найдена", responseCode = "404",
+                            content = @Content(schema = @Schema(implementation = AppError.class))
+                    )
+            })
+    public SessionDto findDyId(@PathVariable @Parameter(
+            description = "Идентификатор сессии", required = true) long id) {
         return sessionConverter.toDto(sessionTracingService.findBySessionId(id));
     }
 
     @GetMapping("/redis/{id}")
-    public SessionDto readFromRedis(@PathVariable long id) {
+    @Operation(summary = "Запрос на получение активной сессии по id",
+            description = "Получение сессии из кэша быстрого доступа, в котором хранятся активные сессии",
+            responses = {
+                    @ApiResponse(
+                            description = "Успешный ответ", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = SessionDto.class))
+                    ),
+                    @ApiResponse(
+                            description = "Сессия не найдена", responseCode = "404",
+                            content = @Content(schema = @Schema(implementation = AppError.class))
+                    )
+            })
+    public SessionDto readFromRedis(@PathVariable @Parameter(
+            description = "Идентификатор сессии", required = true) long id) {
         return sessionConverter.toDto(sessionTracingService.findFromRedis(id));
     }
 
     @GetMapping("/active")
+    @Operation(summary = "Запрос на получение списка активных сессий",
+            description = "Получение списка сессий из кэша быстрого доступа, в котором хранятся активные сессии",
+            responses = {
+                    @ApiResponse(
+                            description = "Успешный ответ", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = Page.class))
+                    )
+            })
     public List<SessionDto> readAllFromRedis() {
         return sessionTracingService.findAllFromRedis()
                 .stream().map(sessionConverter::toDto).collect(Collectors.toList());
     }
 
     @GetMapping("/prolong/{id}")
-    public void prolongSession(@PathVariable Long id) {
+    @Operation(summary = "Продление срока жизни сессии по id",
+            description = "При отсутствии активности сессия автоматически закрывается через 30 минут. " +
+                    "Данный запрос подтверждает наличие активности и восстанавливает срок жизни сессии до начальных 30 минут.",
+            responses = {
+                    @ApiResponse(
+                            description = "Успешный ответ", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = SessionDto.class))
+                    ),
+                    @ApiResponse(
+                            description = "Сессия не найдена или уже закрыта", responseCode = "404",
+                            content = @Content(schema = @Schema(implementation = AppError.class))
+                    )
+            })
+    public void prolongSession(@PathVariable @Parameter(
+            description = "Идентификатор сессии", required = true) Long id) {
         sessionTracingService.prolongSession(id);
     }
 
     @PostMapping
-    public SessionDto create(@RequestBody SessionDto sessionDto) {
+    @Operation(summary = "Запрос на создание новой сессии",
+            responses = {
+                    @ApiResponse(
+                            description = "Сессия успешно создана", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = SessionDto.class))
+                    ),
+                    @ApiResponse(
+                            description = "В запросе на создание сессии не заполнено поле userId", responseCode = "400",
+                            content = @Content(schema = @Schema(implementation = AppError.class))
+                    ),
+                    @ApiResponse(
+                            description = "Открыто слишком много сессий", responseCode = "406",
+                            content = @Content(schema = @Schema(implementation = AppError.class))
+                    )
+            })
+    public SessionDto create(@RequestBody @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "Дапнные, необходимые для создания новой сессии",
+            required = true,
+            content = @Content(schema = @Schema(implementation = SessionDto.class))) SessionDto sessionDto) {
         sessionValidator.validate(sessionDto);
         Session session = sessionConverter.toEntity(sessionDto);
         return sessionConverter.toDto(sessionTracingService.save(session));
     }
 
     @GetMapping("/logout/{id}")
-    public void logout(@PathVariable long id) {
+    @Operation(summary = "Запрос на закрытие сессии",
+            responses = {
+                    @ApiResponse(
+                            description = "Продукт успешно создан", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = SessionDto.class))
+                    ),
+                    @ApiResponse(
+                            description = "Сессия не найдена", responseCode = "404",
+                            content = @Content(schema = @Schema(implementation = AppError.class))
+                    ),
+                    @ApiResponse(
+                            description = "Невозможно выполнить операцию. Сессия с уже закрыта.", responseCode = "406",
+                            content = @Content(schema = @Schema(implementation = AppError.class))
+                    )
+            })
+    public void logout(@PathVariable @Parameter(
+            description = "Идентификатор сессии", required = true) long id) {
         sessionTracingService.logout(id);
     }
 
 }
 
-// TODO Дописать сваггер в SessionDto
 // TODO Посмотреть по поводу операций с сессиями в сервисе и подумать, как их перекинуть на редис
-
-/*
-Обсудить:
-1. Метод делете, код 204
-2. Валидатор
-3. Продление сессии
-4. Удаление старых сессий
-5. Ошибка 404 пользователь не найден
-*/
+// TODO Комменты
+// TODO Отремонтировать кнопки страниц
+// TODO Сделать маскировку ip
+// TODO Тесты
--- session-tracing/src/main/java/com/globus/session_tracing/dtos/SessionDto.java
+++ session-tracing/src/main/java/com/globus/session_tracing/dtos/SessionDto.java
@@ -6,7 +6,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
 @AllArgsConstructor
@@ -16,19 +15,19 @@ import java.util.Date;
 @Schema(description = "Модель записи в таблицу трассировки сессий")
 public class SessionDto {
 
-    @Schema(description = "ID записи", example = "1")
+    @Schema(description = "Идентификатор сессии", example = "1")
     private Long id;
-    @Schema(description = "ID пользователя", example = "1")
+    @Schema(description = "Идентификатор пользователя", example = "1")
     private Integer userId;
     @Schema(description = "Дата и время входа пользователя в аккаунт", example = "2025-06-10T18:15:39.697528")
     private Date loginTime;
     @Schema(description = "Дата и время выхода пользователя из аккаунта", example = "2025-06-10T19:17:39.697528")
     private Date logoutTime;
-    @Schema(description = "Метод входа", example = "?")
+    @Schema(description = "Метод входа", example = "password")
     private String method;
     @Schema(description = "Маскированный ip-адрес", example = "192.168.***.**1")
     private String ipAddress;
-    @Schema(description = "Информация об устройстве", example = "?")
+    @Schema(description = "Информация об устройстве")
     private String deviceInfo;
     @Schema(description = "Активность сессии", example = "true")
     private Boolean isActive;
